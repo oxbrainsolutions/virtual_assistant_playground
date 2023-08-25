c53d2d1805e3fe6f593ea2d056429a46da6ba23c
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-08-25 11:22:57 -0700

    Update app.py

--- app.py
+++ app.py
@@ -565,7 +565,7 @@ st.markdown("""
   font-family: sans-serif;
   height: 3em;
   }
-  div[data-baseweb="textarea"] {
+  div[data-baseweb="textarea"] > div {
   width: 65%;
   }
   div[data-baseweb="textarea"] > div,
