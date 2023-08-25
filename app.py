d803db09874908f38508ce81ea97f5c1359dce2d
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-08-25 16:46:12 -0700

    Update app.py

--- app.py
+++ app.py
@@ -767,7 +767,6 @@ with col2:
       <style>
       @media (max-width: 1024px) {
           p.header_text {
-          margin-top: 0em;
             font-size: 3.2em;
           }
       }
@@ -775,7 +774,7 @@ with col2:
   '''
   st.markdown(header_media_query + header_text, unsafe_allow_html=True)
   information_text1 = '''
-    <p class="information_text" style="margin-top: 2em; margin-bottom: 2em; text-align: justify;"><span style="color: #FAFAFA; font-family: sans-serif; font-size: 1em; ">In this interactive playground, you can explore the capabilities of conversational AI technology using advanced natural language processing algorithms. To begin, simply engage the virtual assistant by asking a question and the AI model will provide you with an insightful response. Please note that this playground is designed to process a maximum of 10 interactions.</span></p>
+    <p class="information_text" style="margin-top: 2em; margin-bottom: 2em; text-align: justify;"><span style="color: #FAFAFA; font-family: sans-serif; font-size: 1em; ">In this interactive playground, you can explore the capabilities of conversational AI technology using advanced natural language processing algorithms. To begin, simply ask the virtual assistant a question and the AI model will respond accordingly. Please note that this playground is designed to process a maximum of 10 interactions.</span></p>
   '''
   subheader_text_field2 = st.empty()
   subheader_text_field2.markdown(information_media_query + information_text1, unsafe_allow_html=True)
